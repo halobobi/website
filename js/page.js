4f4fbe31fd21a8160c321e005fa4120a7845f290
bandita.eu <laandro3@gmail.com>
2024-01-12 02:07:36 +0100

    Changes.

---
+++ js/page.js
@@ -0,0 +1,45 @@
+let isScrolled = false;
+let isMenuOpen = false;
+
+window.onscroll = function() {
+    if(!window.matchMedia("(max-width: 600px)").matches){
+            scrollPC();
+    }else{
+        isMenuOpen=true;
+        toggleMenu();
+    }
+};
+const icon= document.getElementById("menu-icon")
+icon.addEventListener("click",toggleMenu)
+
+function toggleMenu(){
+    const nav = document.querySelector("nav")
+    if(isMenuOpen){
+        nav.classList.add('hidden')
+        isMenuOpen = false
+        icon.innerText="☰";
+
+    }
+    else{
+        nav.classList.remove('hidden')
+        isMenuOpen = true
+        icon.innerText="✖";
+    }
+    
+    
+}
+
+function scrollPC() {
+    const header = document.querySelector("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        if (!isScrolled) {
+            header.classList.add('scroll')
+
+            isScrolled = true;
+        }
+    } else {
+        header.classList.remove('scroll')
+        
+        isScrolled = false;
+    }
+}
