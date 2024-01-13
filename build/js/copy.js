9991782a57284c67d4580dc3389f5cf46c480ad4
bandita.eu <laandro3@gmail.com>
2024-01-13 17:57:16 +0100

    Modified copy.js

--- build/js/copy.js
+++ build/js/copy.js
@@ -1,8 +1,7 @@
 document.getElementById('copy').addEventListener('click',copyResp)
 
-
 function copyResp(){
     navigator.clipboard.writeText(document.getElementById('resp').innerText)
     document.getElementById('success').classList.add('visible')
-    setInterval(()=>{document.getElementById('success').classList.remove('visible')},1000)
+    setTimeout((e)=>{document.getElementById('success').classList.remove('visible')},1100)
 }
