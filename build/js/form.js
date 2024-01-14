3ffcbf77cb91710170b4f1745d37c2c63bb26c91
bandita.eu <laandro3@gmail.com>
2024-01-14 18:51:26 +0100

    Update form.js

--- build/js/form.js
+++ build/js/form.js
@@ -1,10 +1,7 @@
 let emailc = 0;
 
-let emailbodyen
-let emailbodyhu
-
-fetch('https://raw.githubusercontent.com/halobobi/website/master/development/email_en.html').then(response=>{return response.text()}).then(result=>{emailbodyen=result})
-fetch('https://raw.githubusercontent.com/halobobi/website/master/development/email_hu.html').then(response=>{return response.text()}).then(result=>{emailbodyhu=result})
+let emailbodyen=await (await fetch('https://raw.githubusercontent.com/halobobi/website/master/development/email_en.html')).text()
+let emailbodyhu=await (await fetch('https://raw.githubusercontent.com/halobobi/website/master/development/email_hu.html')).text()
 
 addNew()
 
