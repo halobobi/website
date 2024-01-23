580443bf5fd8989431a1d4950457c1fee49c6649
bandita.eu <laandro3@gmail.com>
2024-01-23 19:41:37 +0100

    Update teapot.js

--- build/js/teapot.js
+++ build/js/teapot.js
@@ -1,2 +1,9 @@
+function playSound(){
+    sound.currentTime=0
+    sound.play()
+}
+
 const sound = new Audio('//www.andrasbiro.work/sounds/pour.mp3')
-document.getElementsByTagName('img')[0].addEventListener('click',()=>{sound.currentTime=0;sound.play()})
+
+document.getElementsByTagName('img')[0].addEventListener('click',playSound)
+document.getElementById('details').addEventListener('click',playSound)
